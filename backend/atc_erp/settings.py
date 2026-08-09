f35da05d84ebccaad6959ae60083c9950cec7726
Faisal Khaliq <f.khaliq0@gmail.com>
2026-08-10 03:08:33 +0530

    Complete settings and urls for React frontend

--- backend/atc_erp/settings.py
+++ backend/atc_erp/settings.py
@@ -3,6 +3,7 @@ Django settings for atc_erp project.
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -105,7 +106,10 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
