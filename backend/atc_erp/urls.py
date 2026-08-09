a8da8b5252e0ed5066597784bf0a26901e653dab
Faisal Khaliq <f.khaliq0@gmail.com>
2026-08-09 23:41:43 +0530

    Add root URL with welcome page

--- backend/atc_erp/urls.py
+++ backend/atc_erp/urls.py
@@ -1,23 +1,18 @@
-"""
-URL configuration for atc_erp project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+
+# Welcome page function
+def home(request):
+    return HttpResponse("""
+        <h1>Welcome to ATC ERP</h1>
+        <p>Agent Page: <a href='/agent/'>/agent/</a></p>
+        <p>Menu Page: <a href='/menu/'>/menu/</a></p>
+        <p>Admin: <a href='/admin/'>/admin/</a></p>
+    """)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('inventory.urls')),
+    path('', home),  # This handles the root URL (/)
 ]
