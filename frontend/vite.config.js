4ed21432d73a42695ba9c45c32ca6f4603391006
Faisal Khaliq <f.khaliq0@gmail.com>
2026-08-31 18:38:35 +0530

    Force new file hashes with Vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,9 +4,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
+    outDir: 'dist',
     rollupOptions: {
       output: {
-        // Force new file names
         entryFileNames: 'assets/[name]-[hash].js',
         chunkFileNames: 'assets/[name]-[hash].js',
         assetFileNames: 'assets/[name]-[hash].[ext]'
